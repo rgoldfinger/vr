632df56e419fcf8e14a9d88935e0994a5321f80f
ROGER GOLDFINGER <rgoldfinger@gmail.comm>
2017-10-21 09:42:29 -0700

    initial commit

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Portal : MonoBehaviour {
+
+    [Header("Cameras")]
+    public Camera arCam;
+    public Camera portalCam;
+
+    void Update () {
+        // Move portal camera position based on main camera distance from the portal.
+        Vector3 cameraOffset = arCam.transform.position - transform.position;
+        portalCam.transform.position = transform.position + cameraOffset;
+
+        // Make portal cam face the same direction as the main camera.
+        portalCam.transform.rotation = Quaternion.LookRotation(arCam.transform.forward, Vector3.up);
+    }
+
+    void OnTriggerEnter (Collider other) {
+        if (other.CompareTag ("ARCamera")) {
+            // Use xor operator to toggle the ARWorld layer in the arCam's culling mask.
+            arCam.cullingMask ^= 1 << LayerMask.NameToLayer("ARWorld");
+        }
+    }
+}
---
+++ Assets/Scripts/VREnable.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEngine.VR;
+
+public class VREnable : MonoBehaviour
+{
+    //bool isEnableVR = false;
+    //bool isVRinProgres = false;
+
+    void Start() {
+        SetupVR(true);
+    }
+
+    //void OnGUI()
+    //{
+    //    if (!isVRinProgres && GUI.Button(new Rect((Screen.width) * 0.1f, (Screen.height) * 0.1f, (Screen.width) * 0.1f, (Screen.height) * 0.1f), "VR:" + isEnableVR.ToString()))
+    //    {
+    //        isVRinProgres = true;
+    //        isEnableVR = !isEnableVR;
+    //        SetupVR(isEnableVR);
+    //    }
+    //}
+
+    /// ////////////////  VR CODE here
+
+    public void SetupVR(bool isVR)
+    {
+        Camera[] camers = (Camera[])FindObjectsOfType<Camera>();
+        StartCoroutine(LoadDevice(isVR, camers));
+    }
+
+    IEnumerator LoadDevice(bool isVR, Camera[] camers)
+    {
+        // VRDeviceName none
+        string newDevice = UnityEngine.XR.XRSettings.supportedDevices.Length > 0 ? UnityEngine.XR.XRSettings.supportedDevices[0] : "none";
+        if (isVR)
+        {
+            //VRDeviceName Cardboard or Split
+#if UNITY_EDITOR
+            newDevice = "Split";
+#else
+            newDevice = UnityEngine.XR.XRSettings.supportedDevices.Length > 1 ? UnityEngine.XR.XRSettings.supportedDevices[1] : "none";
+#endif
+        }
+        Debug.Log("VRDeviceName " + newDevice);
+        UnityEngine.XR.XRSettings.LoadDeviceByName(newDevice);
+        yield return null;
+        Debug.Log(UnityEngine.XR.XRSettings.loadedDeviceName + " is loaded");
+#if !UNITY_EDITOR
+        foreach (Camera cam in camers)
+            {
+                if (cam != null)
+                {
+                    UnityEngine.XR.XRDevice.DisableAutoXRCameraTracking(cam, isVR);
+                }
+            }
+        if(Camera.main)
+        UnityEngine.XR.XRDevice.DisableAutoXRCameraTracking(Camera.main, isVR);
+#endif
+        UnityEngine.XR.XRSettings.enabled = isVR;
+        //isVRinProgres = false;
+    }
+    /// ////////////////  VR CODE End
+}
