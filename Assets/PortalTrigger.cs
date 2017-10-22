bd206d542bc27e3223a1b4577f4d391a0567e6e0
ROGER GOLDFINGER <rgoldfinger@gmail.comm>
2017-10-22 16:18:46 -0700

    redo portal

---
+++ Assets/PortalTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTrigger : MonoBehaviour {
+
+    public Camera arCam;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("MainCamera"))
+        {
+            // Use xor operator to toggle the ARWorld layer in the arCam's culling mask.
+            arCam.cullingMask ^= 1 << LayerMask.NameToLayer("ARWorld");
+            arCam.cullingMask ^= 1 << LayerMask.NameToLayer("Plane");
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
