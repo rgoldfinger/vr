8a0283e12e21bbd60aeed8a5fb8cc6df3ea26404
ROGER GOLDFINGER <rgoldfinger@gmail.comm>
2017-10-26 11:50:32 -0700

    triangles

--- Assets/Scripts/Tunnel.cs
+++ Assets/Scripts/Tunnel.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tunnel : MonoBehaviour {
-    public GameObject square;
+    public GameObject triangle;
     // Use this for initialization
     private Color32[] colors = { new Color32(131, 249, 244, 1), new Color32(195, 66, 221, 1) };
     void Start () {
-        InvokeRepeating("CreateThing", 0, 1);
+        InvokeRepeating("CreateThing", 0, 0.5f);
+        for (var i = 10; i < 100; i += 10) {
+            Spawn((i));
+        }
+
     }
 
     void CreateThing () {
-        GameObject thing = Instantiate(square, new Vector3(0.4f, -0.4f, 110), square.transform.rotation);
+        Spawn(110);
+    }
+
+    void Spawn (int loc) {
+        GameObject thing = Instantiate(triangle, new Vector3(0, 0, loc), triangle.transform.rotation);
         thing.GetComponent<TunnelMover>().speed = Random.Range(5, 20);
         Renderer rend = thing.GetComponent<Renderer>();
         rend.material.color = colors[Random.Range(0, 2)];
