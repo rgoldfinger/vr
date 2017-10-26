62e19b98905e7c0eaa9ea3a87e85adeff27c3790
ROGER GOLDFINGER <rgoldfinger@gmail.comm>
2017-10-26 10:16:14 -0700

    start on tunnel

---
+++ Assets/Tunnel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tunnel : MonoBehaviour {
+    public GameObject square;
+    // Use this for initialization
+    private Color32[] colors = { new Color32(131, 249, 244, 1), new Color32(195, 66, 221, 1) };
+    void Start () {
+        InvokeRepeating("CreateThing", 0, 1);
+    }
+
+    void CreateThing () {
+        GameObject thing = Instantiate(square, new Vector3(0.4f, -0.4f, 110), square.transform.rotation);
+        thing.GetComponent<TunnelMover>().speed = Random.Range(5, 20);
+        Renderer rend = thing.GetComponent<Renderer>();
+        rend.material.color = colors[Random.Range(0, 2)];
+    }
+}
---
+++ Assets/TunnelMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TunnelMover : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 t = Camera.main.transform.position + new Vector3(0, 0, -100);
+
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, t, step);
+	}
+}
