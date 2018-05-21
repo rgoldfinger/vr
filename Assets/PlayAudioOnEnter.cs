dc79d0d2af1481dea3ceeafef23e091333654ac4
Roger Goldfinger <rgoldfinger@gmail.com>
2018-05-21 07:15:09 -0700

    working with tracking

---
+++ Assets/PlayAudioOnEnter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudioOnEnter : MonoBehaviour {
+
+	public AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	private void OnTriggerEnter (Collider other) {
+		if (other.CompareTag ("Player"))
+			audio.Play ();
+	}
+}
