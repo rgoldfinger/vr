cc0e04e81b710a4bea5d67d368476bbfe0c8b8fe
Roger Goldfinger <rgoldfinger@gmail.com>
2018-05-24 19:59:03 -0700

    arrange room, fix visability of crystals and ground

--- Assets/Scripts/VREnable.cs
+++ Assets/Scripts/VREnable.cs
@@ -1,15 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
 using System.Collections;
-using System;
+using UnityEngine;
 using UnityEngine.VR;
 
-public class VREnable : MonoBehaviour
-{
+public class VREnable : MonoBehaviour {
     //bool isEnableVR = false;
     //bool isVRinProgres = false;
 
-    void Start() {
-        SetupVR(true);
+    void Start () {
+        SetupVR (true);
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
     }
 
     //void OnGUI()
@@ -24,18 +24,15 @@ public class VREnable : MonoBehaviour
 
     /// ////////////////  VR CODE here
 
-    public void SetupVR(bool isVR)
-    {
-        Camera[] camers = (Camera[])FindObjectsOfType<Camera>();
-        StartCoroutine(LoadDevice(isVR, camers));
+    public void SetupVR (bool isVR) {
+        Camera[] camers = (Camera[]) FindObjectsOfType<Camera> ();
+        StartCoroutine (LoadDevice (isVR, camers));
     }
 
-    IEnumerator LoadDevice(bool isVR, Camera[] camers)
-    {
+    IEnumerator LoadDevice (bool isVR, Camera[] camers) {
         // VRDeviceName none
         string newDevice = UnityEngine.XR.XRSettings.supportedDevices.Length > 0 ? UnityEngine.XR.XRSettings.supportedDevices[0] : "none";
-        if (isVR)
-        {
+        if (isVR) {
             //VRDeviceName Cardboard or Split
 #if UNITY_EDITOR
             newDevice = "Split";
@@ -43,20 +40,18 @@ public class VREnable : MonoBehaviour
             newDevice = UnityEngine.XR.XRSettings.supportedDevices.Length > 1 ? UnityEngine.XR.XRSettings.supportedDevices[1] : "none";
 #endif
         }
-        Debug.Log("VRDeviceName " + newDevice);
-        UnityEngine.XR.XRSettings.LoadDeviceByName(newDevice);
+        Debug.Log ("VRDeviceName " + newDevice);
+        UnityEngine.XR.XRSettings.LoadDeviceByName (newDevice);
         yield return null;
-        Debug.Log(UnityEngine.XR.XRSettings.loadedDeviceName + " is loaded");
+        Debug.Log (UnityEngine.XR.XRSettings.loadedDeviceName + " is loaded");
 #if !UNITY_EDITOR
-        foreach (Camera cam in camers)
-            {
-                if (cam != null)
-                {
-                    UnityEngine.XR.XRDevice.DisableAutoXRCameraTracking(cam, isVR);
-                }
+        foreach (Camera cam in camers) {
+            if (cam != null) {
+                UnityEngine.XR.XRDevice.DisableAutoXRCameraTracking (cam, isVR);
             }
-        if(Camera.main)
-        UnityEngine.XR.XRDevice.DisableAutoXRCameraTracking(Camera.main, isVR);
+        }
+        if (Camera.main)
+            UnityEngine.XR.XRDevice.DisableAutoXRCameraTracking (Camera.main, isVR);
 #endif
         UnityEngine.XR.XRSettings.enabled = isVR;
         //isVRinProgres = false;
